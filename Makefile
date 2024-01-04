fb6a51a731284392f229bd8bd451fd37b173e3cf
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2024-01-05 00:18:09 +0300

    Add crossplatform build pipeline

--- Makefile
+++ Makefile
@@ -3,12 +3,16 @@ CFLAGS = -g
 BIN_DIR = bin
 SRC_DIR = src
 OBJ_DIR = obj
-LDLIBS = -lglfw -lGLEW -lGL
+LDLIBS = -lwavefront
 
-ifeq ($(OS), "Windows_NT")
-	LDFLAGS += -L/mingw64/lib/
+ifeq ($(OS), Windows_NT)
+	LDFLAGS += -L/clang64/bin/
+	CFLAGS += -I/clang64/include/ -DGLFW_DLL
+	LDLIBS += -lglfw3 -lglew32 -lopengl32
 else
 	LDFLAGS += -L/usr/lib/ -L/usr/local/lib
+	CFLAGS += -I/usr/include/
+	LDLIBS += -lglfw -lGLEW -lGL
 endif
 
 .PHONY: clean
@@ -16,6 +20,7 @@ endif
 $(BIN_DIR)/viewer: $(SRC_DIR)/viewer.c
 	@mkdir -p $(@D)
 	$(CC) $(CFLAGS) $(LDFLAGS) $(LDLIBS) -o $@ $<
+	cp /clang64/bin/glfw3.dll /clang64/bin/glew32.dll $(BIN_DIR)/
 
 clean:
 	rm -rf $(BIN_DIR) $(OBJ_DIR)
--- src/viewer.c
+++ src/viewer.c
@@ -1,14 +1,17 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include "wavefront/wavefront.h"
+
 #include <stdio.h>
 
 const char *NAME = "Model Preview";
 const size_t WIDTH = 640;
 const size_t HEIGHT = 480;
 
-int main(void)
-{
+const char *model_filepath = "C:/dev/wavefront-parser/cube.obj";
+
+int main(void) {
     GLFWwindow* window;
 
     if (!glfwInit()) {
@@ -17,20 +20,20 @@ int main(void)
     }
 
     window = glfwCreateWindow(WIDTH, HEIGHT, NAME, NULL, NULL);
-    if (!window)
-    {
+    if (!window) {
         puts("Could not initialize window.");
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
+
     if (glewInit() != GLEW_OK) {
         puts("Could not load glew.");
+        glfwTerminate();
         return -1;
     }
 
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
